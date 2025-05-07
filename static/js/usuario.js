bc3a95b0de9330c05417d42cbee28d65caf1da85
DiegoVazquez <150133186+vazquezdiego@users.noreply.github.com>
2025-05-07 10:51:19 +0200

    he añadido el navbar colapsable a la página de usuario

--- static/js/usuario.js
+++ static/js/usuario.js
@@ -189,3 +189,24 @@ async function verDetalles(viajeId) {
         alert('No se pudieron cargar los detalles del viaje');
     }
 }
+
+
+// Boton colpasar navbar
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleButton = document.getElementById('menu-boton');
+    const sidebar = document.getElementById('sidebar');
+    const mainContent = document.querySelector('.main-content');
+
+    toggleButton.addEventListener('click', function () {
+        sidebar.classList.toggle('minimized');
+
+                // Si el sidebar se minimiza, agrega clase "expanded" al main-content
+                if (sidebar.classList.contains('minimized')) {
+                    mainContent.classList.add('expanded');
+                } else {
+                    mainContent.classList.remove('expanded');
+                }
+        
+    });
+
+});
